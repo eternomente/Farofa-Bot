d8a12292ce1d901b69451277af9f8a817643d72a
BotFarofa <huehukakde3@hotmail.com>
2015-07-25 23:23:13 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1684,8 +1684,8 @@
             },
 
             clearchatCommand: {
-                command: 'bostejarchat',
-                rank: 'mod',
+                command: ['clearchat', 'deletarchat'],
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
