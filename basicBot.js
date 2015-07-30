eaa1b1351a5ad6a98cba40241164420587260cdb
BotFarofa <huehukakde3@hotmail.com>
2015-07-30 03:45:28 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -541,7 +541,7 @@
                         rankInt = 3;
                         break;
                     case "bouncer":
-                        rankInt = 2;
+                        rankInt = 3;
                         break;
                     case "residentdj":
                         rankInt = 1;
